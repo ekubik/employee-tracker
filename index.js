9aac69beedba0b7c72181d48904216a3c597f653
Ewa Kubik <ewa.kubik@hotmail.com>
2022-04-24 22:39:31 +1000

    update employee role

--- index.js
+++ index.js
@@ -122,7 +122,6 @@ const addADepartment = () => {
 
 const addANewRole = () => {
   inquirer.prompt(addNewRoleQuestions).then((results) => {
-    // const data = [results.name, results.salary, results.department_id];
     db.query(
       `INSERT INTO role(title, salary, department_id) VALUES ("${results.title}", "${results.salary}", "${results.department_id}")`,
       (error, results) => {
@@ -156,7 +155,6 @@ const addAnEmployee = () => {
       console.log(err);
     }
     let role = result.map((role) => ({ name: role.title, value: role.id }));
-    ;
     db.query(`SELECT * FROM employee`, async (err, result) => {
       if (err) {
         console.log(err);
@@ -208,6 +206,50 @@ const addAnEmployee = () => {
   });
 };
 
-const updateEmployeeRole = () => {};
+const updateEmployeeRole = () => {
+  db.query(`SELECT * FROM employee`, async (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    let employee = result.map((employee) => ({
+      name: employee.first_name + " " + employee.last_name,
+      value: employee.id,
+    }));
+    db.query(`SELECT * FROM role`, async (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      let role = result.map((role) => ({ name: role.title, value: role.id }));
+      inquirer
+        .prompt([
+          {
+            name: "employee",
+            type: "rawlist",
+            message: "Select an employee to update.",
+            choices: employee,
+          },
+          {
+            name: "new_role",
+            type: "rawlist",
+            message: "What is this employee's new role?",
+            choices: role,
+          },
+        ])
+        .then((response) => {
+          db.query(
+            `UPDATE employee SET ? WHERE ?`,
+            [{ role_id: response.new_role }, { id: response.employee }],
+            (err, result) => {
+              if (err) {
+                console.log(err);
+              }
+              console.log("Employee has been successfully updated");
+              init();
+            }
+          );
+        });
+    });
+  });
+};
 
 init();
